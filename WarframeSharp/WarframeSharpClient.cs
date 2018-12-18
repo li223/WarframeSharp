c4debf4f8e5bc71d3b8f4198349207598bfda017
li223 <li223@users.noreply.github.com>
2018-12-18 07:31:22 +0000

    big whoops

--- WarframeSharp/WarframeSharpClient.cs
+++ WarframeSharp/WarframeSharpClient.cs
@@ -48,7 +48,7 @@ namespace WarframeSharp
         /// <summary>
         /// The full response from each of the other methods, useful if you want all the data with only one call.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<WorldState?> GetWorldStateDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -64,7 +64,7 @@ namespace WarframeSharp
         /// <summary>
         /// Translated News items from the worldstate
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<News>> GetNewsDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -80,7 +80,7 @@ namespace WarframeSharp
         /// <summary>
         /// Events, such as Fomorian Attacks are included here
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<Event>> GetEventsDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -96,7 +96,7 @@ namespace WarframeSharp
         /// <summary>
         /// Description and rewards for Alerts
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<Alert>> GetAlertsDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -112,7 +112,7 @@ namespace WarframeSharp
         /// <summary>
         /// Data about the missions for the current sortie
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<Sortie?> GetSortieDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -128,7 +128,7 @@ namespace WarframeSharp
         /// <summary>
         /// Cycling through different nodes each day, these are a general listing of the nodes that each syndicate will use for the day.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<SyndicateMission>> GetSyndicateMissionsDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -144,7 +144,7 @@ namespace WarframeSharp
         /// <summary>
         /// Information about current Void Fissure missions
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<Fissure>> GetFissuresDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -160,7 +160,7 @@ namespace WarframeSharp
         /// <summary>
         /// Popular Deals, discounts, featured deals.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<FlashSale>> GetFlashSalesDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -176,7 +176,7 @@ namespace WarframeSharp
         /// <summary>
         /// Data on invasion missions, such as estimated completion time, rewards, etc.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<Invasion>> GetInvasionsDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -192,7 +192,7 @@ namespace WarframeSharp
         /// <summary>
         /// The endpoint this method uses is deprecated and only returns a joke response.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         [Obsolete("The endpoint this method uses is deprecated.", false)]
         public async Task<IEnumerable<DarkSector>> GetDarkSectorsDataAsync(PlatformType platform = PlatformType.PC)
@@ -209,7 +209,7 @@ namespace WarframeSharp
         /// <summary>
         /// Information on the current Void Trader offerings, or when he will arrive.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<VoidTrader?> GetVoidTraderDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -225,7 +225,7 @@ namespace WarframeSharp
         /// <summary>
         /// Darvo's Daily Deal details
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<DailyDeal>> GetDarvoDealsAsync(PlatformType platform = PlatformType.PC)
         {
@@ -241,7 +241,7 @@ namespace WarframeSharp
         /// <summary>
         /// Status data for Simaris' Sanctuary
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<SimarisTarget?> GetSancturayDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -257,7 +257,7 @@ namespace WarframeSharp
         /// <summary>
         /// Data on each day and week's conclave challenges
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<IEnumerable<ConclaveChallenge>> GetConclaveChallengesDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -273,7 +273,7 @@ namespace WarframeSharp
         /// <summary>
         /// Data on the Day/night cycle for Cetus on earth
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<Cycle?> GetCetusCycleDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -289,7 +289,7 @@ namespace WarframeSharp
         /// <summary>
         /// The current Earth day/night cycle progress.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<Cycle?> GetEarthCycleDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -305,7 +305,7 @@ namespace WarframeSharp
         /// <summary>
         /// Construction percentages for showing how far constructed the enemy fleets are.
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<ConstructionProgress?> GetConstructionProgressDataAsync(PlatformType platform = PlatformType.PC)
         {
@@ -321,7 +321,7 @@ namespace WarframeSharp
         /// <summary>
         /// The time that the worldstate was last generated
         /// </summary>
-        /// <param name="platform.ToString().ToLower()">The platform.ToString().ToLower() you would like to get the data for</param>
+        /// <param name="platform">The platform you would like to get the data for</param>
         /// <returns></returns>
         public async Task<string> GetTimestampAsync(PlatformType platform = PlatformType.PC)
         {
