bec912cf497e27336625a105daeffd9f8aa2c7c0
Abner <25711694+AbnerSquared@users.noreply.github.com>
2020-10-01 05:45:04 -0500

    Simplify client structure, fix XML issue & add Orb Vallis endpoint (#4) (#5)
    * Simplify client structure & add Orb Vallis endpoint
    
    * Fix XML documentation to use relative path instead

---
+++ WarframeSharp/Objects/OrbVallis.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WarframeSharp
+{
+    public class OrbVallis
+    {
+        [JsonProperty("id")]
+        public string Id { get; internal set; }
+
+        [JsonProperty("expiry")]
+        public string Expiry { get; internal set; }
+
+        [JsonProperty("timeLeft")]
+        public string TimeLeft { get; internal set; }
+
+        [JsonProperty("isWarm")]
+        public bool IsWarm { get; internal set; }
+    }
+}
