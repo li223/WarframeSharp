53d76c4771080a478c275d36371ef301229904b0
sepp.amsl@protonmail.com <wIB@e2czXGVJ>
2020-10-02 23:16:16 +0200

    Added "useful" unit tests, basic refactoring, added a bunch of missing endpoints.

---
+++ WarframeSharp/Objects/EnemyType.cs
@@ -0,0 +1,15 @@
+﻿namespace WarframeSharp
+{
+    /// <summary>
+    /// The type of enemy
+    /// </summary>
+    public enum EnemyType
+    {
+        Orokin = 0,
+        Corrupted = 1,
+        Infested = 2,
+        Corpus = 0,
+        Grineer = 1,
+        Tenno = 2
+    }
+}
